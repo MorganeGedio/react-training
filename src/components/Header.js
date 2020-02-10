2c28e361ec040ad1adeb4850c7c45b5687be3d32
Morgane Gedio <morgane.gedio@gmail.com>
2020-02-10 14:36:01 +0100

    add route and components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
+const Header = props => (
+        <header className="top">
                 <h1>Catch 
                     <span className="ofThe">
                         <span className="of">Of</span>
@@ -11,11 +9,30 @@ class Header extends React.Component {
                     </span>
                     day</h1>
             <h3 className="tagline">
-                <span>Fresh daily</span>
+                {/* props = object in component */}
+                <span>{props.tagline}</span>
             </h3>
             </header>
-        );
-    }
-}
+    );
+
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className="top">
+//                 <h1>Catch 
+//                     <span className="ofThe">
+//                         <span className="of">Of</span>
+//                         <span className="the">The</span>
+//                     </span>
+//                     day</h1>
+//             <h3 className="tagline">
+//                 {/* this = component instance
+//                 .props = object in component */}
+//                 <span>{this.props.tagline}</span>
+//             </h3>
+//             </header>
+//         );
+//     }
+// }
 
 export default Header;
