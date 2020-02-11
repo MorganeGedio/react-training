818f2670c19bc5dfe89f21649e5d7affa7d7107d
Morgane Gedio <morgane.gedio@gmail.com>
2020-02-11 14:04:18 +0100

    handle event with on clclick events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = event => {
+        // Stop form from submitting 
+        event.preventDefault();
+        // get text from input
+        const storeName = this.myInput.current.value;
+        // change page to /store/input
+        this.props.history.push(`/store/${storeName}`);
+    };
     render() { 
         return (
-            <form className="store-selector">   
-                <h2>Please Enter A Store</h2>    
-                <input type="text" required placeholder="Store Name"/>
+            <form className="store-selector" onSubmit={this.goToStore}>   
+                <h2>Please Enter A Store</h2> 
+                <input 
+                    type="text" 
+                    ref={this.myInput}
+                    required 
+                    placeholder="Store Name" 
+                    defaultValue={getFunName()}
+                    />
                 <button type="submit">Visit Store ➡️</button>
             </form>
         );
