d7cee2d0d0a639a6f00d8480abb98b810e6f892e
Morgane Gedio <morgane.gedio@gmail.com>
2020-02-11 15:07:53 +0100

    add a fish form

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,22 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
-
 class App extends React.Component {
+    state = {
+        fishes: {}, 
+        order: {}
+    };
+    addFish = (fish) => {
+        // take copy of existing state (we don't want a mutation)
+        // object spread
+        const fishes = {...this.state.fishes}
+        // add new fish
+        fishes[`fish${Date.now()}`] = fish;
+        // set new fishes object to state - built in method
+        this.setState({
+            fishes: fishes
+        });
+    };
     render() {
         return (
             <div className="catch-of-the-day"> 
@@ -12,7 +26,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Market"/>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         );
     }
