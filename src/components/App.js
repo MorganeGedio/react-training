77d966f02fd107dbf4605e8d12e04ec8c7394295
Morgane Gedio <morgane.gedio@gmail.com>
2020-02-12 14:34:34 +0100

    display order state with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -51,11 +51,15 @@ class App extends React.Component {
                         addToOrder={this.addToOrder}/>)}
                     </ul>
                 </div>
-                <Order />
+                <Order 
+                fishes={this.state.fishes}
+                order={this.state.order}
+                // {...this.state}
+                // OR spread but passing all the data 
+                />
                 <Inventory 
                 addFish={this.addFish} 
                 loadSampleFishes={this.loadSampleFishes}
-
                 />
             </div>
         );
